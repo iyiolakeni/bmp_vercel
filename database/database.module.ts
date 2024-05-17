cd671b289a47ddada9da9d76ebf8bf2750cec922
iyiolakeni <iyiolakeni@gmail.com>
2024-05-17 15:10:17 +0100

    hope it works

--- database/database.module.ts
+++ database/database.module.ts
@@ -1,28 +1,32 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import {User} from '../src/user/entities/user.entity'
-import {Form} from '../src/form/entities/form.entity'
-import { MerchantID } from '../src/merchantID/merchantID.entity';
-import { Email } from '../src/email/email.entity';
-import { Pos } from '../src/pos/pos.entity';
-
-
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "../src/user/entities/user.entity";
+import { Form } from "../src/form/entities/form.entity";
+import { MerchantID } from "../src/merchantID/merchantID.entity";
+import { Email } from "../src/email/email.entity";
+import { Pos } from "../src/pos/pos.entity";
 
 @Module({
   imports: [
-      TypeOrmModule.forFeature([User, Form, MerchantID, Pos, Email]),
-        TypeOrmModule.forRoot({
-      type: 'postgres',
-          host: process.env.POSTGRESS_HOST,
-          port: 5432,
-          username: process.env.POSTGRES_USER,
+    TypeOrmModule.forFeature([User, Form, MerchantID, Pos, Email]),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRESS_HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       entities: [User, Form, MerchantID, Pos, Email],
       synchronize: true,
-          // logging: true,
-        }),
-      ],
-      exports: [TypeOrmModule],
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      // logging: true,
+    }),
+  ],
+  exports: [TypeOrmModule],
 })
 export class DatabaseModule {}
